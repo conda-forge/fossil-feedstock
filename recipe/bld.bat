8851f1e7576651cde6aa5968b543652cb93b9649
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-07-13 11:16:19 -0400

    Ensure scripts %SCRIPTS%, break up nmake args

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,15 @@
 cd win
 python %RECIPE_DIR%\fix_openssl_win.py Makefile.msc
 if errorlevel 1 exit 1
-nmake /f Makefile.msc FOSSIL_DYNAMIC_BUILD=1 FOSSIL_ENABLE_SSL=1 FOSSIL_ENABLE_JSON=1 FOSSIL_ENABLE_TCL=1 FOSSIL_BUILD_ZLIB=0 PERLDIR=%PREFIX%\Library\bin
+nmake /f Makefile.msc ^
+  FOSSIL_DYNAMIC_BUILD=1 ^
+  FOSSIL_ENABLE_SSL=1 ^
+  FOSSIL_ENABLE_JSON=1 ^
+  FOSSIL_ENABLE_TCL=1 ^
+  FOSSIL_BUILD_ZLIB=0 ^
+  PERLDIR=%PREFIX%\Library\bin
 if errorlevel 1 exit 1
+md %SCRIPTS% | echo "%SCRIPTS% already exists"
+dir
 copy fossil.exe %SCRIPTS%\fossil.exe
 if errorlevel 1 exit 1
