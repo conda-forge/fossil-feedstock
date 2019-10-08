4c71be43dab155d65dc5d0398a80d132eee53617
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-10-07 22:22:47 -0400

    update bld.bat with msinttypes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,13 @@
 @echo ON
+
+:: Finds stdint.h from msinttypes.
+set INCLUDE=%LIBRARY_INC%;%INCLUDE%
+
 cd win
+
 python %RECIPE_DIR%\fix_openssl_win.py Makefile.msc
 if errorlevel 1 exit 1
+
 nmake /f Makefile.msc ^
   FOSSIL_DYNAMIC_BUILD=1 ^
   FOSSIL_ENABLE_SSL=1 ^
@@ -10,7 +16,9 @@ nmake /f Makefile.msc ^
   FOSSIL_BUILD_ZLIB=0 ^
   PERLDIR=%PREFIX%\Library\bin
 if errorlevel 1 exit 1
+
 md %SCRIPTS% | echo "%SCRIPTS% already exists"
 dir
+
 copy fossil.exe %SCRIPTS%\fossil.exe
 if errorlevel 1 exit 1
