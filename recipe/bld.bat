c24f79461533d552a076dbf0764e6162071f0ab6
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-10-09 21:14:50 -0400

    skip older vs, remove openssl patch script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,16 +7,12 @@ set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 
 cd win
 
-python %RECIPE_DIR%\fix_openssl_win.py Makefile.msc
-if errorlevel 1 exit 1
-
 nmake /f Makefile.msc ^
   FOSSIL_DYNAMIC_BUILD=1 ^
   FOSSIL_ENABLE_SSL=1 ^
   FOSSIL_ENABLE_JSON=1 ^
   FOSSIL_ENABLE_TCL=1 ^
   FOSSIL_BUILD_ZLIB=0 ^
-  USE_SYSTEM_SQLITE=1 ^
   PERLDIR=%PREFIX%\Library\bin
 if errorlevel 1 exit 1
 
