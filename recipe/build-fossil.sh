bf8c7bbc625b90eb2075cc3fb213a97947310272
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-12-28 14:43:03 -0600

    fix typo, use shellcheck

--- recipe/build-fossil.sh
+++ recipe/build-fossil.sh
@@ -1,34 +1,36 @@
 #!/usr/bin/env bash
 set -eux
 
-CC="$(basename "$CC")"
+# shellcheck disable=SC2155
+export CC="$(basename "${CC}")"
 
-ln -s $BUILD_PREFIX/bin/$CC $BUILD_PREFIX/bin/cc
+ln -s "${BUILD_PREFIX}/bin/${CC}" "${BUILD_PREFIX}/bin/cc"
 
 export PIKCHR_WASM=extsrc/pikchr.wasm
 
-if [[ -f "$PIKCHR_WASM" ]]; then
-  echo "$PIKCHR_WASM exists, not doing any emsdk setup"
+if [[ -f "${PIKCHR_WASM}" ]]; then
+  echo "${PIKCHR_WASM} exists, not doing any emsdk setup"
 else
   # TODO: maybe pass in via script_env?
   emsdk install latest
   emsdk activate latest
-  . ${CONDA_EMSDK_DIR}/emsdk_env.sh
+  # shellcheck disable=SC1091
+  . "${CONDA_EMSDK_DIR}/emsdk_env.sh"
 fi
 
-./configure --prefix=${PREFIX} \
-  --build=${BUILD} \
-  --host=${HOST} \
+./configure --prefix  "${PREFIX}" \
+  --build             "${BUILD}" \
+  --host              "${HOST}" \
+  --with-tcl          "${PREFIX}" \
+  --with-zlib         "${PREFIX}/include" \
+  --with-openssl      "${PREFIX}" \
   --disable-internal-sqlite \
-  --with-tcl=${PREFIX} \
-  --with-zlib=${PREFIX}/include \
-  --with-openssl=${PREFIX} \
   --json \
   CFLAGS="${CFLAGS} -I${PREFIX}/include" \
-  LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
+  LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 # runs without error if already exists
-make -j${CPU_COUNT} wasm
-make -j${CPU_COUNT}
+make "-j${CPU_COUNT}" wasm
+make "-j${CPU_COUNT}"
 
 make install --debug
