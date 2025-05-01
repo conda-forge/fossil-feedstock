c275d3611e982c5d0359aa3c293d99d329182c91
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-05-01 08:20:22 -0500

    always do emscripten setup

--- recipe/build-fossil.sh
+++ recipe/build-fossil.sh
@@ -6,19 +6,13 @@ export CC="$(basename "${CC}")"
 
 ln -s "${BUILD_PREFIX}/bin/${CC}" "${BUILD_PREFIX}/bin/cc"
 
-export PIKCHR_WASM=extsrc/pikchr.wasm
+emsdk install latest
+emsdk activate latest
+# shellcheck disable=SC1091
+. "${CONDA_EMSDK_DIR}/emsdk_env.sh"
 
-if [[ -f "${PIKCHR_WASM}" ]]; then
-  echo "${PIKCHR_WASM} exists, not doing any emsdk setup"
-else
-  # TODO: maybe pass in via script_env?
-  emsdk install latest
-  emsdk activate latest
-  # shellcheck disable=SC1091
-  . "${CONDA_EMSDK_DIR}/emsdk_env.sh"
-fi
-
-./configure --prefix="${PREFIX}" \
+./configure \
+  --prefix="${PREFIX}" \
   --build="${BUILD}" \
   --host="${HOST}" \
   --with-tcl="${PREFIX}" \
